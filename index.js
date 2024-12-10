f5403534345bfe089277ba1f9ec24ee184e8105a
Cocdas <138351304+Cocdas@users.noreply.github.com>
2024-12-10 05:15:21 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import cheerio from "cheerio";
+
+export const scrapeHiruNews = async (req, res) => {
+  try {
+    const pageNumber = parseInt(req.params.page);
+
+    const response = await axios.get(
+      `https://www.hirunews.lk/local-news.php?pageID=${pageNumber}`
+    );
+
+    const $ = cheerio.load(response.data);
+
+    const data = $(".trending-section .row .column:nth-child(3)")
+      .map((index, element) => ({
+        _id: index + 1,
+        title: $(element)
+          .find(".all-section-tittle a:nth-child(2)")
+          .text()
+          .trim(),
+        date: $(element).find(".middle-tittle-time").text().trim(),
+        link: $(element)
+          .find(".all-section-tittle a:nth-child(2)")
+          .attr("href"),
+      }))
+      .get();
+
+    const images = $(".trending-section .row .column:nth-child(1)")
+      .map((index, element) => ({
+        image: $(element).find(".sc-image img").attr("src"),
+      }))
+      .get();
+
+    const combined = data
+      .map((title, i) => ({
+        ...title,
+        ...images[i],
+      }))
+      .map(({ _id, title, date, link, image }) => ({
+        _id,
+        title,
+        date,
+        link,
+        image,
+      }));
+
+    res.json(combined);
+  } catch (error) {
+    console.error(error);
+    res.send("Something went wrong!");
+  }
+};
